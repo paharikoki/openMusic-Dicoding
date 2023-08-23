1e0b63d956b625078250fa14d91e8a8b1e32e16f
Ahmad Alan Lestari <paharikoki@gmail.com>
2023-08-23 21:30:10 +0700

    (-) Remove lowercase

--- src/validator/users/schema.js
+++ src/validator/users/schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const UserPayloadSchema = Joi.object({
   username: Joi.string().required(),
-  password: Joi.string().required().lowercase(),
+  password: Joi.string().required(),
   fullname: Joi.string().required(),
 });
 
